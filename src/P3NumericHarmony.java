609ec410b4f11f197299a4ca47f233b5a669c001
Manjunath <manjunathdtc3@gmail.com>
2024-08-02 23:33:48 +0530

    P3 Numeric Harmony

---
+++ src/P3NumericHarmony.java
@@ -0,0 +1,23 @@
+/*
+Pattern:
+
+1111
+2222
+3333
+4444
+ */
+
+
+public class P3NumericHarmony {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+

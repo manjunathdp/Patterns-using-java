20fbdabd36f776ea9e049c21f7d470ea001be3c0
Manjunath <manjunathdtc3@gmail.com>
2024-08-02 23:19:33 +0530

    Asterisk Symphony

---
+++ src/P1AsteriskSymphony.java
@@ -0,0 +1,23 @@
+/*
+You need to develop a simple Java application to print a given pattern using a loop
+structure.
+Pattern to Print:
+*
+*
+*
+*
+
+
+Print the Given Pattern
+. Use a for loop to iterate 4 times.
+. Inside the loop, print the asterisk ( * ) followed by a newline character.
+ */
+public class P1AsteriskSymphony {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("*");
+
+        }
+
+    }
+}

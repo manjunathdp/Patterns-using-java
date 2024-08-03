364c747d8b9681b4d549e45aa4ef7f59cda38a7f
Manjunath <manjunathdtc3@gmail.com>
2024-08-03 14:37:38 +0530

    P12 Reflective Star Pyramid

---
+++ src/P12ReflectiveStarPyramid.java
@@ -0,0 +1,31 @@
+
+/*
+
+ *
+ **
+ ***
+ ****
+ *****
+ ****
+ ***
+ **
+ *
+
+ */
+public class P12ReflectiveStarPyramid {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <=5-i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}

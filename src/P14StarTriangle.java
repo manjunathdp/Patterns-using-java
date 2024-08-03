e2632d53ebad359a4c2a7090a5b524c64bfad11f
Manjunath <manjunathdtc3@gmail.com>
2024-08-04 01:26:38 +0530

    P14 Star Triangle

---
+++ src/P14StarTriangle.java
@@ -0,0 +1,24 @@
+/*
+
+Pattern:
+  *
+ ***
+*****
+ */
+import java.util.Scanner;
+
+public class P14StarTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int star = 1; star <= ((i*2)-1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

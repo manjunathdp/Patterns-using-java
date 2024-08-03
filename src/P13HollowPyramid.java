5b7a207fc308e652eaa0aebb40c1022ca9e22691
Manjunath <manjunathdtc3@gmail.com>
2024-08-03 15:34:11 +0530

    P13 Hollow Pyramid

---
+++ src/P13HollowPyramid.java
@@ -0,0 +1,19 @@
+public class P13HollowPyramid {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <=4-i ; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if(i>1) {
+                for (int j = 1; j <= ((i - 1) * 2) - 1; j++) {
+                    System.out.print(" ");
+
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}

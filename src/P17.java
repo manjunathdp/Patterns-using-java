9f559394cac40c7cf56556db815100b8eca0976b
Manjunath <manjunathdtc3@gmail.com>
2024-08-07 00:48:53 +0530

    Patterns added

--- src/P17.java
+++ src/P17.java
@@ -4,18 +4,15 @@ public class P17 {
         Scanner sc = new Scanner(System.in);
         System.out.print("input: ");
         int n = sc.nextInt();
-        for (int i = 1; i <=n ; i++) {
-            if (i == 1 || i==n) {
-                for (int j = 1; j <=n ; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=n ; j++) {
+                if (i == 1 || i == n || j == 1 || j==n) {
                     System.out.print("*");
                 }
-            }
-            else {
-                System.out.print("*");
-                for (int j = 1; j <=n-2 ; j++) {
+                else {
                     System.out.print(" ");
                 }
-                System.out.print("*");
+
             }
             System.out.println();
         }
---
+++ src/P18.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class P18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i==n || j==1 || j == i ){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
